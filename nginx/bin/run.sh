84b3bb8373c3e4c3fd3d1dc70b983b69df575e42
Alex Dicianu <alex@getpantheon.com>
2015-01-23 15:44:32 +0200

    Added redis credentials as env variables

--- nginx/bin/run.sh
+++ nginx/bin/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+sed -i "s/REDIS_CACHE_HOST/$REDIS_CACHE_HOST/" /etc/php5/fpm/pool.d/www.conf
+sed -i "s/REDIS_CACHE_PORT/$REDIS_CACHE_PORT/" /etc/php5/fpm/pool.d/www.conf
+
 /etc/init.d/php5-fpm start
 nginx
